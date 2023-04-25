8eba1bd76ffd01799a3ac61cb54bbab6571d7e86
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-04-25 11:59:04 +0200

    Add the deleter to the application so if the user doesn't want to create it is possible to delete them

--- src/main/kotlin/org/luismi/objects/example/application/Application.kt
+++ src/main/kotlin/org/luismi/objects/example/application/Application.kt
@@ -2,7 +2,8 @@ package org.luismi.objects.example.application
 
 import org.luismi.objects.example.asker.contracts.Asker
 import org.luismi.objects.example.asker.contracts.AskerOptions
-import org.luismi.objects.example.creator.contracts.ObjectsCreator
+import org.luismi.objects.example.manage.contracts.ObjectsCreator
+import org.luismi.objects.example.manage.contracts.ObjectsDeleter
 import org.sdi.annotations.Component
 import org.sdi.annotations.Inject
 
@@ -15,11 +16,27 @@ class Application {
     lateinit var asker: Asker
     @Inject
     lateinit var objectsCreator: ObjectsCreator
+    @Inject
+    lateinit var objectsDeleter: ObjectsDeleter
 
     fun init() {
-        when(asker.askForInfo("Create Students as Custom Objects? (y/n)")){
+        when(asker.askForInfo("Do you want to create some Objects? (y/n)")) {
+            AskerOptions.YES -> createObjects()
+            AskerOptions.NO -> deleteObjects()
+        }
+    }
+
+    private fun createObjects() {
+        when(asker.askForInfo("Create Students as Custom Objects? (y/n)")) {
             AskerOptions.YES -> objectsCreator.createObjects(AskerOptions.YES)
             AskerOptions.NO -> objectsCreator.createObjects(AskerOptions.NO)
+        }
+    }
+
+    private fun deleteObjects() {
+        when(asker.askForInfo("Do you want to delete all objects? (y/n)")) {
+            AskerOptions.YES -> objectsDeleter.deleteAllCustomObjectsDefinitions()
+            AskerOptions.NO -> println("Bye Bye!")
         }
     }
 }
